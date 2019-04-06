d6b63f28bb371715dc92cf0df7cad47493fbba6c
Konrad Szałkowski <konrad.szalkowski@gmail.com>
2019-04-06 09:36:58 +0200

    Singleton tests.

---
+++ patterns/src/test/java/com/sda/singleton/SingletonTest.java
@@ -0,0 +1,50 @@
+package com.sda.singleton;
+
+import org.junit.Test;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+import static org.junit.Assert.*;
+
+public class SingletonTest {
+
+    @Test
+    public void shoouldReturnInstance() {
+        // Given
+
+        // When
+        final Singleton singleton = Singleton.getInstance();
+
+        // Then
+        assertNotNull(singleton);
+    }
+
+
+    @Test
+    public void shoouldReturnSameInstance() {
+        // Given
+
+        // When
+        final Singleton singleton1 = Singleton.getInstance();
+        final Singleton singleton2 = Singleton.getInstance();
+
+        // Then
+        assertSame(singleton1, singleton2);
+    }
+
+    @Test
+    public void shouldHavePrivateConstructor() {
+        // Given
+
+        //When
+        Constructor<?>[] constructors = Singleton.class.getConstructors();
+
+
+        // then
+        assertEquals(1, constructors.length);
+        assertFalse(constructors[0].isAccessible());
+
+    }
+
+}
