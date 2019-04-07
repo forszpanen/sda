1ac78bf2002b142045b6658fd66626856b9c6276
Konrad Szałkowski <konrad.szalkowski@gmail.com>
2019-04-07 10:02:48 +0200

    Visitor.

--- patterns/src/main/java/com/sda/visistor/AverageSalaryVisitor.java
+++ patterns/src/main/java/com/sda/visistor/AverageSalaryVisitor.java
@@ -3,17 +3,21 @@ package com.sda.visistor;
 import java.math.BigDecimal;
 
 public class AverageSalaryVisitor implements Visitor {
+    private BigDecimal sum = BigDecimal.ZERO;
+    private int counter = 0;
+
     public BigDecimal getAvgSalary() {
-        return null;
+        return sum.divide(BigDecimal.valueOf(counter));
     }
 
     @Override
     public void visitWorker(Worker worker) {
-
+        sum = sum.add(worker.getSalary());
+        counter++;
     }
 
     @Override
     public void visitCompany(Company company) {
-
+        // No salary in company
     }
 }
